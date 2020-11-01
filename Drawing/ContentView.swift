dba7aae6dbfc06233aee73195c2857858e8713cb
Amber Spadafora <ahelenespadafora@gmail.com>
2020-11-01 01:48:02 -0400

    Project 9 of 100 days of swift ui

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,9 +8,93 @@
 
 import SwiftUI
 
+struct Flower: Shape {
+    var petalOffset: Double = -20
+    
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8 ) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width/2))
+            
+            let rotatedPetal = originalPetal.applying(position)
+            path.addPath(rotatedPetal)
+            
+        }
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+    var insetAmount: CGFloat = 0
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: clockwise)
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        return path
+    }
+}
+
 struct ContentView: View {
+    // for Flower shape
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
-        Text("Hello, World!")
+        
+//        Triangle()
+//            .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//            .frame(width: 150, height: 150)
+//        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+//            .stroke(Color.blue, lineWidth: 10)
+//            .frame(width: 300, height: 300)
+//        Circle()
+//            .strokeBorder(Color.blue, lineWidth:  10)
+        
+        // The Flower Shape demo
+//        VStack {
+//            Flower(petalOffset: petalOffset, petalWidth: petalWidth).fill(Color.red, style: FillStyle(eoFill: true))
+//            Text("Offset")
+//            Slider(value: $petalOffset, in: -40...40).padding([.horizontal, .bottom])
+//            Text("Width")
+//            Slider(value: $petalWidth, in: 0...100).padding(.horizontal)
+//        }
+        
+        Capsule().strokeBorder(ImagePaint(image: Image("example"), scale: 0.1), lineWidth: 20)
+            .frame(width: 300, height: 200)
+        
+        
+        
+        
     }
 }
 
