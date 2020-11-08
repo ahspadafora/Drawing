ada7155c3d04564a6c67dd178de00a880a2f6e38
Amber Spadafora <ahelenespadafora@gmail.com>
2020-11-08 01:15:18 -0500

    Add Trapezoid and Checkerboard shapes

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -64,12 +64,93 @@ struct Triangle: Shape {
     }
 }
 
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0,y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount,y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount,y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX,y: rect.maxY))
+        return path
+    }
+}
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let rowSize = rect.height / CGFloat(rows)
+        let columnsSize = rect.width / CGFloat(columns)
+        
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnsSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+                    let rect = CGRect(x: startX, y: startY, width: columnsSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        
+        return path
+    }
+}
 struct ContentView: View {
     // for Flower shape
     @State private var petalOffset = -20.0
     @State private var petalWidth = 100.0
     
+    @State private var colorCycle = 0.0
+    
+    // For Trapezoid shape
+    @State private var insetAmount: CGFloat = 50
+    
+    // For Checkerboard
+    @State private var rows = 4
+    @State private var columns = 4
+    
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+    
+    
     var body: some View {
+        Checkerboard(rows: rows, columns: columns).onTapGesture {
+            withAnimation(.linear(duration: 3)) {
+                self.rows = 8
+                self.columns = 16
+            }
+        }
+        
+        
+//        Trapezoid(insetAmount: insetAmount).frame(width: 200, height: 100).onTapGesture {
+//            withAnimation {
+//                self.insetAmount = CGFloat.random(in: 10...90)
+//            }
+//        }
+        
+//        VStack {
+//            ColorCyclingCircle(amount: self.colorCycle)
+//                .frame(width: 300, height: 300)
+//            Slider(value: $colorCycle)
+//        }
         
 //        Triangle()
 //            .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
@@ -89,9 +170,9 @@ struct ContentView: View {
 //            Slider(value: $petalWidth, in: 0...100).padding(.horizontal)
 //        }
         
-        Capsule().strokeBorder(ImagePaint(image: Image("example"), scale: 0.1), lineWidth: 20)
-            .frame(width: 300, height: 200)
-        
+//        Capsule().strokeBorder(ImagePaint(image: Image("example"), scale: 0.1), lineWidth: 20)
+//            .frame(width: 300, height: 200)
+//
         
         
         
